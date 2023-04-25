8ad61e134ead586dbf237da7289d58c953080cde
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-25 09:07:00 -0300

    certeza de tudo menos os end

--- src/app/music/spote/endpoints.js
+++ src/app/music/spote/endpoints.js
@@ -1,4 +1,6 @@
 
     
     export const end1= "https://open.spotify.com/artist/2vvraaenFfG1UEwF0Gl5v1"
-    
+    export const url =  "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
+export const possivelerro="https://open.spotify.com/album/3rHHZXuSkY3kpLbCG2e4J7"
+  
