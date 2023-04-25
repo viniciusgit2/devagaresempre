a0d9cbce0cd9d1f60d196ca6ccdcc011cbf0cd5b
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-25 10:12:39 -0300

    certeza de tudo menos o config.js

--- src/app/music/spote/album.js
+++ src/app/music/spote/album.js
@@ -1,11 +1,10 @@
 
 import {tojson} from "util"
 
-import { api_url } from "./config"
+import { api_url,headher } from "./config"
 
-export const getalbum=()=> fetch( `${api_url}()https://api.spotify.com/`)
+export const getalbum=()=> fetch( `${api_url},headher(tojson`)
 .then(tojson)
-export const getalbumtracks=()=>`${api_url}()https://api.spotify.com/`
+export const getalbumtracks=()=>`${api_url}(tojson)`
 .then(tojson)
-export const getalbums=()=>`${api_url}()https://api.spotify.com/`
-.then(tojson)
+export const getalbums=()=>`${api_url}(tojson)`
