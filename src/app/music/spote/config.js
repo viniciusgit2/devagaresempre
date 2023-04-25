c9ce21a9bffd947d433b5c3a9aca5a2f5124b96b
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-25 08:21:31 -0300

    certeza de tudo menos os end

--- src/app/music/spote/config.js
+++ src/app/music/spote/config.js
@@ -1,3 +1,4 @@
+import { end1 } from "./endpoints"
 export const url =   ` https://api.spotify.com/v1/albums/{id}`
 export const headher={
     headher:{
--- src/app/music/spote/endpoints.js
+++ src/app/music/spote/endpoints.js
@@ -1,3 +1,4 @@
 
     
     export const end1= "https://open.spotify.com/artist/2vvraaenFfG1UEwF0Gl5v1"
+    
